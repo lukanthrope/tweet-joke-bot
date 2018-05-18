c441b8c3d3bce474f0e608d1672fcb0218cae3b5
lukanthrope <bydlocoder228@gmail.com>
2018-05-19 01:58:19 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-var twit = require('twit');
-
-module.exports = {
-    consumer_key: '6QJ5S910HMmHf5vZcrn2guyQc',
-    consumer_secret: 'k3fFNlgp1baMgDjj44YABrb7HXjoyy20FTbE5mzSXoNCiALL5Y',
-    access_token: '997547152187514881-QGYDJ598sVJU02BeGsHzIT6uLUc9YaF',
-    access_token_secret: '6QXMw2y3WQU9hADTUAk4nujgvbDGVEcIuWPnidRJ1jhLw',
-}
